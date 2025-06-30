778ad9bb14664659e6c22802f98e887ea3aa0589
Abdul-Aleem Mohammed <abaleemmohammed@gmail.com>
2025-06-30 13:13:22 -0500

    added the messing-around page and figured out the audio playing feature

---
+++ messing-around.js
@@ -0,0 +1,8 @@
+let sound = document.getElementById("audio-section");
+let audioButton = document.querySelector(".audio-button");
+
+
+audioButton.addEventListener("click", function(){
+    sound.play();
+})
+
