cf5171e2d415fd2a607b1b8360f3e1102a213c57
Abdul-Aleem Mohammed <abaleemmohammed@gmail.com>
2025-06-30 14:54:14 -0500

    added image carousel to experience page

--- exp-script.js
+++ exp-script.js
@@ -1,13 +1,59 @@
-// made for card 1, should be able to copy and paste for all of the cards and just change the numbers
+// image carousel for exp 1
 
-let card1Button = document.querySelector(".more-info-button-card1");
-let expandedCard1 = document.querySelector(".expanded-card1");
-let card1CloseButton = document.querySelector(".expanded-card-close-button")
+let currentCardNumber = 0;
+let cardChangeButton1 = document.querySelector(".image-carousel-button1");
+let cardChangeButton2 = document.querySelector(".image-carousel-button2");
+let cardChangeButton3 = document.querySelector(".image-carousel-button3");
+let card1 = document.querySelector(".image-carousel-card1");
+let card2 = document.querySelector(".image-carousel-card2");
+let card3 = document.querySelector(".image-carousel-card3");
 
-card1Button.addEventListener("click", function(){
-    expandedCard1.style.display = "block";
+function cardCheck(){
+    if (currentCardNumber === 0) {
+        card1.style.display = "flex";
+        card2.style.display = "none";
+        card3.style.display = "none";
+    }
+    if (currentCardNumber === 1) {
+        card1.style.display = "none";
+        card2.style.display = "flex";
+        card3.style.display = "none";
+    }
+    if (currentCardNumber === 2) {
+        card1.style.display = "none";
+        card2.style.display = "none";
+        card3.style.display = "flex";
+    }
+}
+
+
+cardChangeButton1.addEventListener("click", function(){
+    currentCardNumber = currentCardNumber +1;
+    if (currentCardNumber >2){   
+        // so if you want more cards, just change this condition to desired number of cards -1
+        currentCardNumber = 0;
+    }
+    cardCheck();
 });
 
-card1CloseButton.addEventListener("click", function(){
-    expandedCard1.style.display = "none";
-})
+cardChangeButton2.addEventListener("click", function(){
+    currentCardNumber = currentCardNumber +1;
+    if (currentCardNumber >2){   
+        // so if you want more cards, just change this condition to desired number of cards -1
+        currentCardNumber = 0;
+    }
+    cardCheck();
+});
+
+cardChangeButton3.addEventListener("click", function(){
+    currentCardNumber = currentCardNumber +1;
+    if (currentCardNumber >2){   
+        // so if you want more cards, just change this condition to desired number of cards -1
+        currentCardNumber = 0;
+    }
+    cardCheck();
+});
+
+cardCheck();
+
+// image carousel for exp 1
